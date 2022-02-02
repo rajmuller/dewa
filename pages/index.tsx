28291053698bc73ecbc58d8a062b5e63cef6d07a
adamRajmuller <rajfta@gmail.com>
2022-02-02 09:28:58 +0100

    chore: contact

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import { GetStaticProps } from "next";
 import { FC } from "react";
 import { useRouter } from "next/router";
-import { Button } from "@chakra-ui/react";
+import Image from "next/image";
+import { chakra, Flex, Heading, Text } from "@chakra-ui/react";
 
 import { ChevronLeftIcon } from "../components/icons";
 import { getContentSlugs } from "../util";
+import { Button } from "../components/uikit";
+import { useContact } from "../hooks";
 
 type IndexProps = {
   slugs: string[];
@@ -12,28 +15,30 @@ type IndexProps = {
 
 const Index: FC<IndexProps> = ({ slugs }) => {
   const router = useRouter();
+  const { onOpen } = useContact();
 
   return (
-    <>
-      {slugs.map((slug) => {
-        const buttonText = slug.replace("-", " ");
-        return (
-          <Button
-            key={slug}
-            m="4"
-            rightIcon={<ChevronLeftIcon ml={20} boxSize={4} />}
-            onClick={() => {
-              router.push({
-                pathname: "/posts/[slug]",
-                query: { slug },
-              });
-            }}
-          >
-            {buttonText}
-          </Button>
-        );
-      })}
-    </>
+    <Flex w="100vw" h="80vh" overflow="hidden">
+      <Flex flex={1} direction="column" h="100%" justify="center">
+        <Heading fontSize={64}>
+          Mi festjük a jö<chakra.span color="red.500">w</chakra.span>őt
+        </Heading>
+        <Text mt={4} mb={[12, 12, 32]}>
+          Magyarország piacvezető festékipari megoldásai
+        </Text>
+        <Button onClick={onOpen} bg="primary.500" variant="primary">
+          Írjon Nekünk
+        </Button>
+      </Flex>
+      <Flex flex={3} h="100%" position="relative">
+        <Image
+          objectFit="contain"
+          layout="fill"
+          src="/hero.png"
+          alt="Festekszoro kep"
+        />
+      </Flex>
+    </Flex>
   );
 };
 

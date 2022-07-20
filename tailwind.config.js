319c1f0193e3614acec1e9775697d02175521a11
adamRajmuller <rajfta@gmail.com>
2022-07-20 08:25:45 +0200

    tailwind init

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      maxWidth: {
+        default: "1280px",
+      },
+      colors: {
+        background: "#F1F5FF",
+      },
+    },
+  },
+  plugins: [],
+};

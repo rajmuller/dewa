d26a77b5ceaf048a70ce2de5c557c95b053e4811
adamRajmuller <rajfta@gmail.com>
2022-07-28 11:54:10 +0200

    contact bekescsaba and maps

--- pages/kapcsolat/index.tsx
+++ pages/kapcsolat/index.tsx
@@ -144,7 +144,13 @@ const ContactList: FC<ContactProps> = ({ contacts }) => {
       <Flex direction="column" alignSelf="normal" mt={8}>
         {contacts.map((contact) => {
           return (
-            <Flex p={4} direction="column" mb={8} shadow="sm">
+            <Flex
+              key={contact.slug}
+              p={4}
+              direction="column"
+              mb={8}
+              shadow="sm"
+            >
               <Text fontWeight={500} fontSize="18" mb={2}>
                 {contact.nev}
               </Text>
@@ -165,76 +171,79 @@ const ContactList: FC<ContactProps> = ({ contacts }) => {
   }
 
   return (
-    <Table {...getTableProps()}>
-      <Thead>
-        {headerGroups.map((headerGroup) => (
-          <Tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column) => {
-              return (
-                <Th
-                  display={
-                    isMd && isMobile && column.id === "department"
-                      ? "none"
-                      : "th"
-                  }
-                  key={headerGroup}
-                  {...column.getHeaderProps(column.getSortByToggleProps())}
-                  isNumeric={column.isNumeric}
-                >
-                  {getIcon(column.id)}
-                  {column.render("Header")}
-                  <>
-                    {/* eslint-disable-next-line no-nested-ternary */}
-                    {column.isSorted ? (
-                      column.isSortedDesc ? (
-                        <chakra.span pl="4">
-                          <ChevronDownIcon aria-label="sorted descending" />
-                        </chakra.span>
-                      ) : (
-                        <chakra.span pl="4">
-                          <ChevronDownIcon
-                            transform="rotate(180deg)"
-                            aria-label="sorted ascending"
-                          />
-                        </chakra.span>
-                      )
-                    ) : null}
-                  </>
-                </Th>
-              );
-            })}
-          </Tr>
-        ))}
-      </Thead>
-      <Tbody {...getTableBodyProps()}>
-        {rows.map((row, i) => {
-          prepareRow(row);
-          return (
-            <Tr
-              key={row}
-              bg={i % 2 === 0 && "primary.100"}
-              {...row.getRowProps()}
-            >
-              {row.cells.map((cell) => {
+    <div className="h-72 w-full overflow-auto">
+      <Table {...getTableProps()}>
+        <Thead>
+          {headerGroups.map((headerGroup) => (
+            <Tr key={headerGroup.id} {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => {
                 return (
-                  <Td
+                  <Th
                     display={
-                      isMd && isMobile && cell.column.id === "department"
+                      isMd && isMobile && column.id === "department"
                         ? "none"
-                        : "td"
+                        : "th"
                     }
-                    {...cell.getCellProps()}
-                    isNumeric={cell.column.isNumeric}
+                    key={headerGroup}
+                    {...column.getHeaderProps(column.getSortByToggleProps())}
+                    isNumeric={column.isNumeric}
                   >
-                    {cell.render("Cell")}
-                  </Td>
+                    {getIcon(column.id)}
+                    {column.render("Header")}
+                    <>
+                      {/* eslint-disable-next-line no-nested-ternary */}
+                      {column.isSorted ? (
+                        column.isSortedDesc ? (
+                          <chakra.span pl="4">
+                            <ChevronDownIcon aria-label="sorted descending" />
+                          </chakra.span>
+                        ) : (
+                          <chakra.span pl="4">
+                            <ChevronDownIcon
+                              transform="rotate(180deg)"
+                              aria-label="sorted ascending"
+                            />
+                          </chakra.span>
+                        )
+                      ) : null}
+                    </>
+                  </Th>
                 );
               })}
             </Tr>
-          );
-        })}
-      </Tbody>
-    </Table>
+          ))}
+        </Thead>
+        <Tbody {...getTableBodyProps()}>
+          {rows.map((row, i) => {
+            prepareRow(row);
+            return (
+              <Tr
+                key={row}
+                bg={i % 2 === 0 && "primary.100"}
+                {...row.getRowProps()}
+              >
+                {row.cells.map((cell) => {
+                  return (
+                    <Td
+                      key={cell.value}
+                      display={
+                        isMd && isMobile && cell.column.id === "department"
+                          ? "none"
+                          : "td"
+                      }
+                      {...cell.getCellProps()}
+                      isNumeric={cell.column.isNumeric}
+                    >
+                      {cell.render("Cell")}
+                    </Td>
+                  );
+                })}
+              </Tr>
+            );
+          })}
+        </Tbody>
+      </Table>
+    </div>
   );
 };
 
@@ -259,17 +268,32 @@ const Contact: FC<ContactProps> = ({ contacts }) => {
           Békéscsaba
         </Option>
       </HStack>
-      {/* TODO: bekescsaba kozpont */}
       <ContactList contacts={selectedContacts} />
-      <Heading mt={32} mb={6} fontSize={24} fontWeight={400}>
-        2040 Budaörs, Gyár u. 2. (Budaörsi Ipari Park)
-      </Heading>
+      {active === "budaors" ? (
+        <Heading mt={32} mb={6} fontSize={24} p={4} fontWeight={400}>
+          2040 Budaörs, Gyár u. 2. (Budaörsi Ipari Park)
+        </Heading>
+      ) : (
+        <Heading mt={32} mb={6} fontSize={24} p={4} fontWeight={400}>
+          5600 Békéscsaba Gyulai út (Lukoil-al szemben)
+        </Heading>
+      )}
       <AspectRatio w="100%" ratio={[1, 1, 31 / 9]}>
-        <iframe
-          loading="lazy"
-          title="Dewa HQ"
-          src="https://www.google.com/maps/embed/v1/search?q=dewa%20hungary&key=AIzaSyDGQJAOWTOczUfw4RU9YoRBdpD44Kas81Q"
-        />
+        {active === "budaors" ? (
+          <iframe
+            loading="lazy"
+            title="Dewa HQ"
+            allowFullScreen
+            src="https://www.google.com/maps/embed/v1/place?q=dewa&key=AIzaSyDGQJAOWTOczUfw4RU9YoRBdpD44Kas81Q&zoom=14"
+          />
+        ) : (
+          <iframe
+            title="Dewa Bekescsaba"
+            loading="lazy"
+            allowFullScreen
+            src="https://www.google.com/maps/embed/v1/place?q=Nagyr%C3%A9t%201422%2C%20B%C3%A9k%C3%A9scsaba%2C%205600&key=AIzaSyDGQJAOWTOczUfw4RU9YoRBdpD44Kas81Q&zoom=14"
+          />
+        )}
       </AspectRatio>
     </Flex>
   );
